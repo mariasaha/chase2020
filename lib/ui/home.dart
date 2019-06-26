3279054a3de29f01feb90c737c02d6d180622734
maria saha <mariasaha@marias-MacBook-Pro.local>
2019-06-26 09:40:47 -0400

    initial

---
+++ lib/ui/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Welcome to Chase 2020'),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue,
+      ),
+      //start the body
+      body: new Center(
+
+      ),
+    );
+  }
+
+}
